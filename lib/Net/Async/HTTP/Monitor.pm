07d8a85bcaa441b39ab437d1c0333e22e3b60a7d
Kent Fredric <kentfredric@gmail.com>
2015-12-03 08:00:42 +1300

    initial commit

---
+++ lib/Net/Async/HTTP/Monitor.pm
@@ -0,0 +1,17 @@
+use 5.006;  # our
+use strict;
+use warnings;
+
+package Net::Async::HTTP::Monitor;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT: Kent Failed To Provide An Abstract
+
+# AUTHORITY
+
+use Moo;
+
+no Moo;
+
+1;
