f656d7e249aa2e845da2fc36711e76b18ba3fff2
Kent Fredric <kentfredric@gmail.com>
2015-12-04 02:12:32 +1300

    Remove dependency on class::tiny

--- example/metacpan.feed.pl
+++ example/metacpan.feed.pl
@@ -16,27 +16,33 @@ my $ua   = Net::Async::HTTP->new();
 $loop->add($ua);
 
 use Data::Dump qw(pp);
+
+my @uris = (
+  'https://metacpan.org/feed/recent',
+  'http://kentfredric.github.io/'
+);
+for my $i ( 0..$#uris) {
 my $monitor = Net::Async::HTTP::Monitor->new(
   http             => $ua,
-  refresh_interval => 10,
-  uri              => URI->new('https://metacpan.org/feed/recent'),
+  refresh_interval => 60,
+  uri              => URI->new($uris[$i]),
   on_updated       => sub {
     my ($response) = @_;
-    *STDERR->print( "Full Response: \n" . ( length $response->content ) . "\n" );
+    *STDERR->print( "<$i>: Full Response: \n" . ( length $response->content ) . "\n" );
   },
   on_updated_chunk => sub {
     my ( $response, @chunk ) = @_;
-    *STDERR->print( "Got new content!: " . substr( ( join q[], @chunk ), 0, 10 ) . "\n" );
+    *STDERR->print( "<$i>: Got new content!: " . substr( ( join q[], @chunk ), 0, 10 ) . "\n" );
   },
   on_no_content => sub {
-    *STDERR->print("No content check!\n");
+    *STDERR->print("<$i>:No content check!\n");
   },
   on_error => sub {
-    *STDERR->print( "Got error!\n" . join q[], pp(@_) );
+    *STDERR->print( "<$i>:Got error!\n" . join q[], pp(@_) );
   },
 );
 $monitor->start;
 $loop->add($monitor);
-
+}
 $loop->run;
 
